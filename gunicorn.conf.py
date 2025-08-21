7b3ebf87a7b097a16c37a0656880147d2389face
RonitKiranMurmu <ronitkiranmurmu@gmail.com>
2025-08-21 15:43:14 +0530

    fixed2

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,9 @@
 workers = 3
-bind = "0.0.0.0:8000"
+bind = "0.0.0.0:$PORT"
 timeout = 120  # Increased to handle blockchain mining
 worker_class = "gevent"  # Use gevent for WebSocket support
 loglevel = "info"
+
+def post_fork(server, worker):
+    from app import init_mongo  # Import init_mongo from app.py
+    init_mongo()  # Initialize MongoDB after fork
